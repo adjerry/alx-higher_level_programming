f1098cbe3354fc8794eeafeeb1d5e01409f66ce5
adjerry <adjerusa@gmail.com>
2022-12-01 14:16:32 +0000

    task

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 9):
-for j in range(i + 1, 10):
-if i == 8:
-    print("{}{}".format(i, j))
+for i in range(0, 100):
+if i == 99:
+    print("{}".format(i))
 else:
-    print("{}{}".format(i, j), end=", ")
+    print("{:02}".format(i), end=", ")
