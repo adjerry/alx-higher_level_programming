830b4df0e95a2054d3e56c40e489b3431c464ed3
adjerry <adjerusa@gmail.com>
2023-02-14 11:02:17 -0800

    task

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,4 +1,2 @@
--- Deletes the database hbtn_0c_0 from the MySQL server,
--- do nothing if the hbtn_0c_0 database doesn't exist.
-
-DROP DATABASE IF EXISTS `hbtn_0c_0`;
+-- Deletes the database hbtn_0c_0 from the MySQL server
+DROP DATABASE IF EXISTS hbtn_0c_0;
