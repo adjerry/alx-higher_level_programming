2e922b9e315cc1cec58d0f67185aa98e39513fb4
adjerry <adjerusa@gmail.com>
2023-02-15 08:48:15 -0800

    task

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,9 +6,9 @@ use 'hbtn_0d_usa';
 
 
 CREATE TABLE IF NOT EXISTS 'cities' (
-       	'id' INT NOT NULL AUTO_INCREMENT UNIQUE,
-	'state_id' INT NOT NULL,
-	'name' VARCHAR(256) NOT NULL,
-	PRIMARY KEY ('id'),
-	FOREIGN KEY ('state_id') REFERENCES 'states'('id')
+'id' INT NOT NULL AUTO_INCREMENT UNIQUE,
+'state_id' INT NOT NULL,
+'name' VARCHAR(256) NOT NULL,
+PRIMARY KEY ('id'),
+FOREIGN KEY ('state_id') REFERENCES 'states'('id')
 )
