8ae201825de5f7e3d467fb72e4cd963ea8d05bcf
adjerry <adjerusa@gmail.com>
2023-02-15 08:41:55 -0800

    task

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,8 @@
+--  lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+
+
+SELECT s.title, g.genre_id
+FROM tv_shows s
+INNER JOIN tv_show_genres g
+ON s.id = g.show_id
+ORDER BY s.title, g.genre_id ASC;
