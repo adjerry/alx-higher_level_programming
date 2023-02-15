3b38a323ee13556536d921e9f92772643ca3a0f1
adjerry <adjerusa@gmail.com>
2023-02-15 08:30:33 -0800

    task

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+
+
+SELECT s.title, g.name
+FROM (tv_shows s LEFT JOIN tv_show_genres sq ON s.id = sg.show_id)
+LEFT JOIN tv_genres g ON sg.genre_id = g.id
+ORDER BY s.title, g.name ASC;
