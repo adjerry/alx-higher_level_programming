e55b7b5f984a648380b3cd53896c4181efdf4f51
adjerry <adjerusa@gmail.com>
2023-02-15 08:33:51 -0800

    task

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,6 +2,6 @@
 
 
 SELECT s.title, g.name
-FROM (tv_shows s LEFT JOIN tv_show_genres sq ON s.id = sg.show_id)
+FROM (tv_shows s LEFT JOIN tv_show_genres sg ON s.id = sg.show_id)
 LEFT JOIN tv_genres g ON sg.genre_id = g.id
 ORDER BY s.title, g.name ASC;
