45c03c0f1c8d73ca5584382ea572a3df0b267967
adjerry <adjerusa@gmail.com>
2023-02-16 02:11:34 -0800

    task

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+-- lists all shows from hbtn_0d_tvshows_rate by their rating.
+
+
+SELECT s.title, SUM(r.rate) AS rating
+FROM tv_shows s
+JOIN tv_show_ratings r ON s.id + r.show_id
+GROUP BY s.title
+ORDER BY rating DESC;
---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,8 @@
+-- lists all genres in the database hbtn_0d_tvshows_rate by their rating
+
+
+SELECT g.name, SUM(r.rate) AS rating
+FROM (tv_genres g JOIN tv_show_genres sg ON g.id = sg.genre_id)
+JOIN tv_show_rating r ON sg.show_id = r.show_id
+GROUP BY g.name
+ORDER BY rating DESC;
