99b026f475e03380af8daa1aac9b1c8e1d48d758
adjerry <adjerusa@gmail.com>
2023-03-25 10:40:36 -0700

    task

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
-'''
+"""
 Lists all State objects, and corresponding City objects
 from the database hbtn_0e_100_usa
-'''
+"""
 
-from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import sys
 from relationship_state import Base, State
 from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-if __name__ == "__main__":
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-              format(argv[1], argv[2], argv[3],
-                  pool_pre_ping=True))
+    st = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
 
-     Session = sessionmaker(bind=engine)
-     session = Session()
-
-     for state in session.query(State).order_by(State.id):
-         print('{}: {}'.format(state.id, state.name))
-         for city in state.cities:
-             print('\t{}: {}'.format(city.id, city.name))
-
-    session.close()
+    for state in st:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name))
--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
-"""prints all City objects
-from the database hbtn_0e_14_usa"""
+"""
+prints all City objects
+from the database hbtn_0e_14_
+"""
+
+from sys import argv
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 if __name__ == "__main__":
-    import sys
-    from model_state import Base, State
-    from model_city import City
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sqlalchemy.schema import Table
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-            .format(sys.argv[1], sys.argv[2],
-                sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    session = Session(engine)
-    for state, city in session.query(State, City)\
-            .filter(City.state_id == State.id)\
-            .order_by(City.id).all():
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    """
+    Access to the database and get the cities
+    from the database.
+    """
+
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    query = session.query(City, State).join(State)
+
+    for _c, _s in query.all():
+        print("{}: ({:d}) {}".format(_s.name, _c.id, _c.name))
+
+    session.commit()
     session.close()
