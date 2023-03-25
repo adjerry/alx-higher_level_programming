19ac4a80395d8830c5db6109192f32d0a4acd62b
adjerry <adjerusa@gmail.com>
2023-03-25 10:24:05 -0700

    task

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -16,9 +16,10 @@ if __name__ == "__main__":
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
             argv[1], argv[2], argv[3])
     engine = create_engine(db_uri)
-    
     Session = sessionmaker(bind=engine)
+    
     session = Session()
+    
     ari_state = session.query(State).filter(State.id == '2').first()
     ari_state.name = 'New Mexico'
     session.commit()
