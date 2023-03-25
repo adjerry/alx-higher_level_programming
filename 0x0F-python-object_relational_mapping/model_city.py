c4b6617ab0b05f39965ae49e20f39ed171a90871
adjerry <adjerusa@gmail.com>
2023-03-25 10:46:02 -0700

    task

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-'''
+"""
 Class definition of a City
-'''
+"""
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import
-
 
 Base = declarative_base()
 
 
 class City(Base):
-    '''
+    """
     City class inherits from Base
-    '''
+    """
     __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True, nullable=False)
